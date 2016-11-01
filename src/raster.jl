953d7a066495b9fb833d048a561a920c15a8fe18
Chris Russ <christoph.russ@roames.com.au>
2016-11-01 14:31:27 +1000

    ABL-333 extending rasterize_points and Raster immutable

--- src/raster.jl
+++ src/raster.jl
@@ -6,8 +6,16 @@ Returns a dictionary containing the indices points that are in a cell.
 """
 immutable Raster <: Associative
     pixels::Dict{Tuple{Int,Int}, Vector{Int}}
+    r_min::Array{Float64,2}
+    r_max::Array{Float64,2}
+    cellsize::Float64
 end
 
+"""
+`rasterize_points{T <: AbstractVector}(points::Vector{T}, dx::AbstractFloat) -> Raster`
+
+Returns `Raster` of  points `pos` with quadratic cellsize `dx`
+"""
 function rasterize_points{T <: AbstractVector}(points::Vector{T}, dx::AbstractFloat)
     min_xy = SVector{3, Float64}(minimum(map(x->x[1], points)), minimum(map(x->x[2], points)), 0) # TODO do this better!
     pixels = Dict{Tuple{Int, Int}, Vector{Int}}()
@@ -22,7 +30,9 @@ function rasterize_points{T <: AbstractVector}(points::Vector{T}, dx::AbstractFl
             push!(pixels[key], i)
         end
     end
-    return Raster(pixels)
+    p_min = minimum(points, 2)
+    p_max = maximum(points, 2)
+    return Raster(pixels,p_min,p_max,dx)
 end
 
 function rasterize_points{T <: Number}(points::Matrix{T}, dx::AbstractFloat)
